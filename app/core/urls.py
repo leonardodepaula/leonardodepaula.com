da695446815de6ca123e5a8bb4dec89f319660fc
Leonardo de Paula Liebscher <leonardo.de.paula@rfb.gov.br>
2021-10-17 11:21:34 -0300

    Dark mode

--- app/core/urls.py
+++ app/core/urls.py
@@ -2,13 +2,16 @@
 from django.urls import path
 
 # Views
-from core.views import IndexView
+from core.views import *
 
 app_name = 'core'
 
 urlpatterns = [
     
     # Index
-    path('',  IndexView.as_view(), name='monitora-index'),
+    path('',  IndexView.as_view(), name='index'),
+
+    # Dark Mode
+    path('dark-mode/',  DarkModeView.as_view(), name='dark-mode'),
 
 ]
--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,7 @@
 # Django
-from django.views.generic  import TemplateView
+from django.views.generic  import TemplateView, View
+from django.http import HttpResponseRedirect
+from django.core.exceptions import PermissionDenied
 
 ###############
 #### INDEX ####
@@ -8,3 +10,30 @@ from django.views.generic  import TemplateView
 class IndexView(TemplateView):
 
     template_name = "core/index.html"
+
+###########################
+#### Session Variables ####
+###########################
+
+class DarkModeView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        raise PermissionDenied()
+
+    def post(self, request, *args, **kwargs):
+
+        try:
+
+            dark_mode = bool(int(request.POST.get('dark-mode')))
+
+            if dark_mode:
+                request.session['dark_mode'] = True
+            else:
+                request.session['dark_mode'] = False
+        
+        except:
+
+            pass
+            
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
